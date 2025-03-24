9defb63febbcaa67daa5fad0d00ad7611b34c557
mateusznasewicz <mateusznasewicz@gmail.com>
2025-03-24 09:11:07 +0100

    poprawa kodu

--- zad1.py
+++ zad1.py
@@ -6,11 +6,10 @@ df = pd.read_csv('Dane_zadanie_1.txt', delimiter='\t')
 for col in df.columns[3:]:
     df[col] = df[col].str.replace(',', '.').astype(float)
 
-df_osoba = df.copy()
-df_osoba['Średnia'] = df_osoba.iloc[:, 3:].mean(axis=1)
-df_osoba['Wariancja'] = df_osoba.iloc[:, 3:-1].var(axis=1)
-df_osoba['Odchylenie'] = df_osoba.iloc[:, 3:-2].std(axis=1)
-df_osoba = df_osoba[['Imię','Nazwisko','Średnia','Wariancja','Odchylenie']]
+df_osoba = df[['Imię','Nazwisko']].copy()
+df_osoba['Średnia'] = df.iloc[:, 3:].mean(axis=1)
+df_osoba['Wariancja'] = df.iloc[:, 3:].var(axis=1)
+df_osoba['Odchylenie'] = df.iloc[:, 3:].std(axis=1)
 
 df_przedmiot = df.iloc[:, 3:].agg(['mean', 'var', 'std']).transpose().reset_index()
 df_przedmiot.columns = ['Przedmiot', 'Średnia', 'Wariancja', 'Odchylenie']
@@ -19,9 +18,9 @@ klasy_oceny = dict.fromkeys(df['Klasa'].unique())
 df_klasa = pd.DataFrame({'Klasa':klasy_oceny.keys()})
 for index, klasa in enumerate(klasy_oceny.keys()):
     oceny = df[df['Klasa'] == klasa].iloc[:, 3:].values.flatten()
-    df_klasa.at[index,'Średnia'] = oceny.mean()
-    df_klasa.at[index,'Wariancja'] = oceny.var()
-    df_klasa.at[index,'Odchylenie'] = oceny.std()
+    df_klasa.loc[index,'Średnia'] = oceny.mean()
+    df_klasa.loc[index,'Wariancja'] = oceny.var()
+    df_klasa.loc[index,'Odchylenie'] = oceny.std()
 
 print(df_osoba, end='\n\n')
 print(df_przedmiot, end='\n\n')
