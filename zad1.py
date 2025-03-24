55179f94a17390e468f171c51a11e9f101f38abd
mateusznasewicz <mateusznasewicz@gmail.com>
2025-03-24 09:06:44 +0100

    skonczone

--- zad1.py
+++ zad1.py
@@ -1,31 +1,34 @@
 # Mateusz Nasewicz
 import pandas as pd
 
-df = pd.read_csv("Dane_zadanie_1", sep="\t")
-estymatory = ["Średnia","Wariancja","Odchylenie"]
-klasy = list(set(df["Klasa"]))
-przedmioty = df.drop(columns=["Imię","Nazwisko","Klasa"]).columns.values
+df = pd.read_csv('Dane_zadanie_1.txt', delimiter='\t')
 
-osoby_df = pd.DataFrame(columns=["Imię","Nazwisko"]+estymatory)
-przedmioty_df = pd.DataFrame(columns=["Przedmiot"]+estymatory)
-klasy_df = pd.DataFrame(columns=["Klasa"]+estymatory)
+for col in df.columns[3:]:
+    df[col] = df[col].str.replace(',', '.').astype(float)
 
-osoby_df["Imię"] = df["Imię"]
-osoby_df["Nazwisko"] = df["Nazwisko"]
-przedmioty_df["Przedmiot"] = przedmioty
-klasy_df["Klasa"] = klasy
+df_osoba = df.copy()
+df_osoba['Średnia'] = df_osoba.iloc[:, 3:].mean(axis=1)
+df_osoba['Wariancja'] = df_osoba.iloc[:, 3:-1].var(axis=1)
+df_osoba['Odchylenie'] = df_osoba.iloc[:, 3:-2].std(axis=1)
+df_osoba = df_osoba[['Imię','Nazwisko','Średnia','Wariancja','Odchylenie']]
 
-# Przedmioty
-for przedmiot in przedmioty:
-    przedmioty_df[przedmiot]["Średnia"] = df[przedmiot].mean()
-    przedmioty_df[przedmiot]["Wariancja"] = 0
-    przedmioty_df[przedmiot]["Odchylenie"] = 0
+df_przedmiot = df.iloc[:, 3:].agg(['mean', 'var', 'std']).transpose().reset_index()
+df_przedmiot.columns = ['Przedmiot', 'Średnia', 'Wariancja', 'Odchylenie']
 
-# Klasy
-for klasa in klasy:
-    pass
+klasy_oceny = dict.fromkeys(df['Klasa'].unique())
+df_klasa = pd.DataFrame({'Klasa':klasy_oceny.keys()})
+for index, klasa in enumerate(klasy_oceny.keys()):
+    oceny = df[df['Klasa'] == klasa].iloc[:, 3:].values.flatten()
+    df_klasa.at[index,'Średnia'] = oceny.mean()
+    df_klasa.at[index,'Wariancja'] = oceny.var()
+    df_klasa.at[index,'Odchylenie'] = oceny.std()
+
+print(df_osoba, end='\n\n')
+print(df_przedmiot, end='\n\n')
+print(df_klasa)
 
 
-# Osoby
+
+
 
 
